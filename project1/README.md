988fcb85b4a1a0f59e69785cccf5b27cc54800b6
Aiden <630479624@qq.com>
2019-10-23 14:53:05 +0800

    项目第一天下班打卡

--- project1/README.md
+++ project1/README.md
@@ -10,3 +10,30 @@
 1. git add .
 2. git commit -m "提交信息"
 3. git push
+
+##制作完成APP组件
+1. 完成Header 区域， 使用的是Mint-UI 中的Header组件
+2. 制作底部的 Tabbar 区域， 使用的是 MUI 的 Tabbar.html 
+ + 在制作购物车 小图标的时候 操作会相对多一些
+ + 先把扩展图标的css样式拷贝到项目中
+ + 拷贝 扩展字体库 ttf  文件到项目中
+ + 为购物车小图标添加样式
+
+3. 要在中间区域放置一个 router-view 展示路由匹配到的组件
+
+## 改造 tabbar 为 router-link
+
+## 这只路由高亮
+
+## 点击tabbar 中的路由链接，展示对应的路由组件
+
+## 制作轮播图布局
+
+## 加载首页轮播图数据
+1. 获取数据，如何获取数据？ 使用vue-resource
+2. 使用 vue-resource 的 this.$http.get 获取数据
+3. 获取到的数据，要保存到data身上
+4. 使用 v-for 循环渲染 每个item 项
+
+## 改造九宫格区域样式
+
--- project1/src/router.js
+++ project1/src/router.js
@@ -1,9 +1,20 @@
 
 import VueRouter from 'vue-router'
 
+import HomeContainer from './components/tabbar/HomeContainer.vue'
+import MemberContainer from './components/tabbar/MemberContainer.vue'
+import SearchContainer from './components/tabbar/SearchContainer.vue'
+import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
+
 var router = new VueRouter({
     routes: [
-    ]
+        {path:'/',component:HomeContainer},
+        {path:'/home',component:HomeContainer},
+        {path:'/member',component: MemberContainer},
+        {path:'/shopcar',component:SearchContainer},
+        {path:'/search',component:ShopcarContainer},
+    ],
+    linkActiveClass: 'mui-active'//覆盖默认的路由高亮的类，默认的类叫做“router-active”
 })
 
 export default router
