5606a13f9f65e4039cd171f23c12e5ff4bf306f5
Aiden <630479624@qq.com>
2019-10-23 10:55:59 +0800

    修改了readme 和 idex.js  及 app.vue

--- project1/src/App.vue
+++ project1/src/App.vue
@@ -1,6 +1,29 @@
 <template>
-  <div>
-    <h1>这是APP组件</h1>
+  <div class="app-container">
+    <!-- 顶部header区域 -->
+    <mt-header fixed title="Vue商城项目"></mt-header>
+
+    <!-- 中间的路由router-view 区域 -->
+
+    <!-- 底部的 tabbar 区域-->
+    <nav class="mui-bar mui-bar-tab">
+			<a class="mui-tab-item mui-active" href="#tabbar">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-chat">
+				<span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+				<span class="mui-tab-label">消息</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-contact">
+				<span class="mui-icon mui-icon-contact"></span>
+				<span class="mui-tab-label">通讯录</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-map">
+				<span class="mui-icon mui-icon-gear"></span>
+				<span class="mui-tab-label">设置</span>
+			</a>
+		</nav>
   </div>
 </template>
 
@@ -8,8 +31,10 @@
 
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.app-container {
+  padding-top: 40px; 
+}
 
 </style>
 
